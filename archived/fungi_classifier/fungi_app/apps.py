4d3ce08e6a995ce3065f16992b81a16c707e4220
Viktor Modroczky <v.modroczky@gmail.com>
2022-04-16 14:05:02 +0200

    removed unused archived code

--- archived/fungi_classifier/fungi_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from tensorflow.keras.models import load_model
-
-class FungiAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fungi_app'
-    model_path = '../models/final_models/vgg16.h5'
-    model = load_model(model_path)
--- archived/fungi_classifier/fungi_app/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Image(models.Model):
-	file = models.ImageField(upload_to="", null=True, blank=True)
